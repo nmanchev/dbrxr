8cdacaf9a101f80368fc09a869c50d0d1df631dc
Nikolay Manchev <nick@manchev.org>
2024-04-12 08:20:14 +0100

    Initial commit

---
+++ dbrxr/__init__.py
@@ -0,0 +1,304 @@
+# Author: Nikolay Manchev <nick@manchev.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Init."""
+__version__ = "0.0.1"
+__author__ = "nikolay.manchev@databricks.com"
+
+import requests
+import logging
+import json
+import time
+
+class ContextNotSetException(Exception):
+    pass
+
+class DBRXCluster(object):
+    
+    def __init__(self, api_url, api_token, polling_int_sec=1, rpy2="check"):
+
+        rpy2_types = ["yes", "no", "check"]
+        if rpy2 not in rpy2_types:
+           raise ValueError("Invalid rpy2 type. Expected one of: %s" % rpy2_types)
+
+        self._api_url = api_url
+        # Set token and headers for API calls
+        self._api_token = api_token
+        self._headers = {
+            "Authorization": f"Bearer {api_token}",
+            "Content-Type": "application/json"
+        }
+
+        self._cluster_id = None
+        self._context = None
+
+        self.polling_int_sec = polling_int_sec
+        
+        self.log = logging.getLogger(__name__)
+
+        if rpy2 == "yes":
+            self.rpy2 = True
+        elif rpy2 == "no":
+            self.rpy2 = False
+        else:
+            self.rpy2 = None
+
+    @property
+    def api_token(self):
+        return self._api_token
+
+    @api_token.setter
+    def api_token(self, token):
+        self._api_token = token
+        self._headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+
+    @property
+    def cluster_id(self):
+        return self._cluster_id
+
+    @cluster_id.setter
+    def cluster_id(self, cluster_id):
+        self._cluster_id = cluster_id
+
+    @property
+    def api_url(self):
+        return self._api_url
+
+    @api_url.setter
+    def api_token(self, api_url):
+        self._api_url = api_url
+
+    def create_context(self, context:str)->bool:
+
+        if self._context:
+            self.log.error(f"Context {self._context} already exists. If you want to create a new context destroy the existing context first.")
+        else:
+            body = {
+                "language": "python",
+                "clusterId": self._cluster_id,
+                "name": context
+            }
+
+            self.log.info(f"Creating Python execution context {context} on cluster {self._cluster_id}...")
+
+
+            # Execute the API call to create the context
+            response = requests.post(f"{self._api_url}/contexts/create", headers=self._headers, data=json.dumps(body))
+
+            # Check the response status code
+            if response.status_code == 200:
+                self._context = json.loads(response.text)["id"]
+                self.log.info(f"Execution context created with ID: {self._context}")
+
+                # Do we need to check for rpy2?
+                if self.rpy2 is None:
+                    self.log.info("rpy2 is set to 'check'. We need to check if the package is installed in the context.")
+                    self.rpy2 = self._python_package_installed("rpy2")
+
+                return True
+            else:
+                self.log.error(f"Error creating execution context: {response.text}")
+
+        return False
+
+
+    def install_py_package(self, package:str)->bool:
+
+        self.log.info(f"Installing Python package {package} in context {self._context}.")
+
+        if (self._python_package_installed(package)):
+        
+            return True
+        
+        else:
+            code = f"""import subprocess
+                       subprocess.check_output(['pip', 'install', {package}])"""
+            
+            self._execute(code)
+            self.log.info(f"Checking if Python package {package} installation succeeded.")
+            return self._python_package_installed(package)
+            
+        return False
+
+    def install_R_package(self, package:str)->bool:
+
+        self.log.info(f"Installing R package {package} in context {self._context}.")
+
+        if (self._r_package_installed(package)):
+           return True       
+        else:
+            code = f"""import subprocess
+
+                      subprocess.check_output(['pip', 'install', {package}])"""
+            
+            self._execute(code)
+            self.log.info(f"Checking if Python package {package} installation succeeded.")
+            return self._package_installed(package)
+            
+        return False
+
+    def _r_package_installed(self, package:str)->bool:
+ 
+        #code=f"""("{package}" %in% rownames(installed.packages()))"""
+
+        code = f"""
+                import rpy2.robjects as robjects
+                res = robjects.r('''"{package}" %in% rownames(installed.packages())''')
+                print(res.r_repr())"""
+
+        res = self._execute(code)
+
+        if res:
+            res_type = res["results"]["resultType"]
+            if (res_type == "text") and (res["results"]["data"] == "TRUE"):
+                self.log.info(f"Success. Package {package} is installed in context {self._context}.")
+                return True
+            elif res_type == "error":
+                self.log.info(f"Failure. Package {package} is NOT installed in context {self._context}.")
+                return False 
+            else:
+                self.log.warn(f"Can't parse the response from the execution.\n {res}")
+                raise
+        else:
+            self.log.warn("Didn't get response from the execution.")
+            raise
+
+    def _python_package_installed(self, package:str)->bool:
+        
+        self.log.info(f"Checking if package {package} is installed in context {self._context}.")
+
+        code = f"""
+        try:
+            import {package}
+            print("Success")
+        except ImportError as e:
+            raise
+        """
+
+        res = self._execute(code)
+        if res:
+            res_type = res["results"]["resultType"]
+            if (res_type == "text") and (res["results"]["data"] == "Success"):
+                self.log.info(f"Success. Package {package} is installed in context {self._context}.")
+                return True
+            elif res_type == "error":
+                self.log.info(f"Failure. Package {package} is NOT installed in context {self._context}.")
+                return False 
+            else:
+                self.log.warn(f"Can't parse the response from the execution.\n {res}")
+                raise
+        else:
+            self.log.warn("Didn't get response from the execution.")
+            raise
+
+    def execute_R(self, r_code:str)->str:
+
+        if self._context is None:
+            self.log.error(f"Context doesn't exist. Can't execute commands.")
+            raise ContextNotSetException
+
+        if not self.rpy2:
+            self.log.error("Can't execute R code if rpy2 isn't installed in the context.")
+            return False
+        
+        code = f"""
+        import rpy2.robjects as robjects
+        res = robjects.r('''{r_code}''')
+        res.r_repr()"""
+
+        return self._execute(code)
+
+    def _execute(self, command:str)->str:
+        
+        if self._context is None:
+            self.log.error(f"Context doesn't exist. Can't execute commands.")
+            raise ContextNotSetException
+        
+        if not command:
+            self.log.error(f"Command is empty. Can't execute.")
+
+        # Set the request body
+        body = {
+            "language": "python",
+            "clusterId": self._cluster_id,
+            "command": command,
+            "contextId": self._context
+        }
+
+        # Execute the API call
+        response = requests.post(f"{self._api_url}/commands/execute", headers=self._headers, data=json.dumps(body))
+
+        # Check the response status code
+        if response.status_code == 200:
+            run_id = json.loads(response.text)["id"]
+            self.log.info(f"Command submitted with run ID: {run_id}")
+        else:
+            self.log.error(f"Error executing command: {response.text}")
+
+        if run_id is not None:
+            
+            # Get the status of the command
+            body.pop("command")
+            body["commandId"] = run_id
+            status = "Running"
+
+            while status == "Running" or status == "Queued":
+
+                self.log.info(f"Command {run_id} is in {status} state...")
+                time.sleep(self.polling_int_sec)
+
+                response = requests.get(f"{self.api_url}/commands/status", headers=self._headers, params=body)
+
+                if response.status_code == 200:
+                    command_info = json.loads(response.text)
+                    status = command_info["status"]
+                else:
+                    self.log.error(f"Error retrieving command {run_id} info: {response.text}")
+
+            # Execution completed. Log the outcome.
+            self.log.info(f"Command {run_id} completed with status: {status}")
+            
+            if status == "Finished":
+                # Only return the results on successful completion
+                return command_info
+
+        return  None
+
+
+    def destroy_context(self)->bool:
+
+        if self._context is None:
+            self.log.error(f"Context is not set. I can't destroy it.")
+            raise ContextNotSetException
+
+        # Set the request body
+        body = {
+            "clusterId": self._cluster_id,
+            "contextId": self._context
+        }
+
+        response = requests.post(f"{self._api_url}/contexts/destroy", headers=self._headers, data=json.dumps(body))
+
+        # Check the response status code
+        if response.status_code == 200:
+            self.log.info(f"Execution context {self._cluster_id} destroyed.")
+            return True
+        else:
+            self.log.error(f"Error creating execution context: {response.text}")
+
+        return False
+
---
+++ demo.py
@@ -0,0 +1,90 @@
+# Author: Nikolay Manchev <nick@manchev.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import logging
+
+from dbrxr import DBRXCluster
+
+def main():
+
+    # Set up logging
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+    logging_level = logging.getLevelName(LOG_LEVEL)
+    logging.basicConfig(level=logging_level)
+    log = logging.getLogger(__name__)
+
+    api_token = os.environ.get("DBRX_API_TOKEN")
+    databricks_host = os.environ.get("DBRX_HOST")
+    cluster_id = os.environ.get("CLUSTER_ID")
+    
+    api_version = "1.2"
+    api_url = databricks_host + "/api/" + api_version
+
+    if (api_token == None):
+        log.error("DBRX_API_TOKEN is None. No cluster id specified.")
+        log.error("Program can't conitnue without a valid API token. This is needed to authenticate to the Databricks cluster.")
+        sys.exit(1)
+
+    cluster = DBRXCluster(api_url, api_token)
+    cluster.cluster_id = cluster_id
+
+    # Create execution context
+    context_name = "my_execution_context"
+    if not cluster.create_context(context_name):
+        log.error("Can't create execution context. Program will terminate.")
+        sys.exit(1)
+
+    # Make sure MLflow is in the context
+    cluster.install_R_package("mlflow")
+
+    # Let's try a model trained on the Wine Quality dataset, which is already stored in MLflow
+    # Update this with a relevant runid for your own model
+    wine_quality_runid = "8c4da65d6bb147d2b9bfdff009dbc8eb"
+    
+    # Now let's write some R code that will generate some "unseen" data and call the model for inference
+    r_code = f'''library(mlflow)
+
+    # Create some test data
+    column_names <- c("fixed.acidity", "volatile.acidity", "citric.acid", "residual.sugar", "chlorides", "free.sulfur.dioxide", "total.sulfur.dioxide", "density", "pH", "sulphates", "alcohol")
+    data_matrix <- matrix(data =  c(7.4,0.7,0,1.9,0.076,11,34,0.9978,3.51,0.56,9.4,
+                                    7.8,0.88,0,2.6,0.098,25,67,0.9968,3.2,0.68,9.8,
+                                    7.8,0.76,0.04,2.3,0.092,15,54,0.997,3.26,0.65,9.8,
+                                    11.2,0.28,0.56,1.9,0.075,17,60,0.998,3.16,0.58,9.8,
+                                    7.4,0.7,0,1.9,0.076,11,34,0.9978,3.51,0.56,9.4),
+                                    nrow = 5, ncol = length(column_names))
+    colnames(data_matrix) <- column_names
+    df <- as.data.frame(data_matrix)
+
+    # Connect to MLflow and pull the model. Inject the Databricks host & token for MLflow authentication
+    Sys.setenv(DATABRICKS_HOST = "{databricks_host}", "DATABRICKS_TOKEN" = "{api_token}")
+
+    model_uri <- "runs:/{wine_quality_runid}/model"
+    best_model <- mlflow_load_model(model_uri = model_uri)
+
+    # Score the data
+    predictions <- data.frame(mlflow_predict(best_model, data = df))          
+    toString(predictions[[1]])
+    '''
+
+    # Run the R code and print the predictions
+    predictions = cluster.execute_R(r_code)
+    print(predictions["results"]["data"])
+
+    # Destroy execution context
+    cluster.destroy_context()
+
+if __name__ == '__main__':
+    main()
